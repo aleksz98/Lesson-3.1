0050cce2dbf4d32574acf02e4162d69e31cf3f1a
Aleksz <aleksznoob@gmail.com>
2023-05-03 18:19:43 +0200

    Initial Commit

---
+++ Lesson 3.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 3.1
+//
+//  Created by Иван on 03.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
